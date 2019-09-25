49d6e15e69667bef9aad5feecda81d42f0ff6015
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-09-25 12:14:13 -0400

    Use the util provided to handle input/printing, gitignore compiled files

--- 2/interp.ml
+++ 2/interp.ml
@@ -1,24 +1,32 @@
+
+
 (* The second homework from Arjun Guha's (UMass) Fall 2018 CS631 'Programming-Languages' class *)
 (* https://people.cs.umass.edu/~arjun/courses/compsci631-fall2018/hw/interp.pdf *)
 
 (* Notes 
 	Command slash comments a line
 	To build and run:
-	ocamlbuild -use-ocamlfind -pkg compsci631 -pkg ppx_test main.d.byte; ./main.d.byte
+	ocamlbuild -use-ocamlfind -pkg compsci631 -pkg ppx_test interp.d.byte; ./interp.d.byte
 	Ocaml runs everything from top down, main method not necessary
+	alt g is goto line
 *)
 
 (* This is necessary for let%TEST to work. *)
 module PTest = Ppx_test.Test
 
+open Interp_util
+
 (* TODO Long match on full language *)
+let interp (e : exp) : exp =
+	match e with
+		| _ -> e
+		| _ -> failwith "not yet implemented"
 
-
-(* Read the input in the format
-	./interp.d.byte program
+(* Read the input in the format ./interp.d.byte program
 	Where program is a filepath to a program in our grammar
+	Assume a single line program (No \n, more than 0 chars)
 *)
-
+let _ = print_endline (show_exp (interp (from_file Sys.argv.(1))))
 
 (* TODO lots of tests *)
 
