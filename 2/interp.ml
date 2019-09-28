2dc54fb12b7fece39dc69c1717a276a7ad7b55c3
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-09-28 19:29:54 -0400

    Added environment rho to interp, wrote basic id tests

--- 2/interp.ml
+++ 2/interp.ml
@@ -9,6 +9,7 @@
 	ocamlbuild -use-ocamlfind -pkg compsci631 -pkg ppx_test interp.d.byte; ./interp.d.byte
 	Ocaml runs everything from top down, main method not necessary
 	alt g is goto line
+	So far this language is immutable, eg it uses rho but not sigma from the lecture 4
 *)
 
 (* This is necessary for let%TEST to work. *)
@@ -16,8 +17,14 @@ module PTest = Ppx_test.Test
 
 open Interp_util
 
-(* TODO Long match on full language *)
-let interp (e : exp) : exp =
+(* Env is an environment in which an expression exists
+	Which maps any number of named ids to values
+	id and const both come from interp_utils
+ *)
+type env = (id * const) list
+
+(* TODO Long match on full language, deciding not to use optional args *)
+let interp (e : exp) (r : env) : exp =
 	match e with
 		| Id x -> failwith "not yet implemented"
 		| Const c -> failwith "not yet implemented"
@@ -39,8 +46,19 @@ let interp (e : exp) : exp =
 	Where program is a filepath to a program in our grammar
 	Assume a single line program (No \n, more than 0 chars)
 *)
-let _ = print_endline (show_exp (from_file Sys.argv.(1)))
-let _ = print_endline (show_exp (interp (from_file Sys.argv.(1))))
+(* let _ = print_endline (show_exp (from_file Sys.argv.(1)))
+let _ = print_endline (show_exp (interp (from_file Sys.argv.(1)))) *)
 
 (* TODO lots of tests *)
+let test_interp_throws (e : exp) (r : env) : bool =
+	try let _ = interp e r in false
+	with _ -> true;;
 
+let%TEST "Unbound id is invalid" = test_interp_throws (Id "x") ["y", Int 3]
+let%TEST "Bound id is that id's value" = interp (Id "x") ["x", Int 3] = Const (Int 3)
+
+
+
+(* Runs all tests declared with let%TEST. This must be the last line
+   in the file. *)
+let _ = Ppx_test.Test.collect ()
