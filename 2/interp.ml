29f4826adb709fef7f30c819b57494e9a1b6a5b1
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-09-29 01:46:13 -0400

    Wrote tests for Id, Const, If, Op2

--- 2/interp.ml
+++ 2/interp.ml
@@ -54,8 +54,61 @@ let test_interp_throws (e : exp) (r : env) : bool =
 	try let _ = interp e r in false
 	with _ -> true;;
 
-let%TEST "Unbound id is invalid" = test_interp_throws (Id "x") ["y", Int 3]
+(* -------ID--------- *)
+let%TEST "Free identifier is invalid" = test_interp_throws (Id "x") ["y", Int 3]
 let%TEST "Bound id is that id's value" = interp (Id "x") ["x", Int 3] = Const (Int 3)
+
+(* -------Const------ *)
+let%TEST "Constant number is that number" = interp (Const (Int 0)) [] = Const (Int 0)
+let%TEST "Constant true is true" = interp (Const (Bool true)) [] = Const (Bool true)
+let%TEST "Constant false is false" = interp (Const (Bool false)) [] = Const (Bool false)
+
+(* --------Op2------- *)
+let%TEST "2 number addition works" = interp (Op2 (Add, Const (Int 3), Const (Int 4))) [] = Const (Int 7)
+let%TEST "2 number subtraction works" = interp (Op2 (Sub, Const (Int 3), Const (Int 4))) [] = Const (Int (-1))
+let%TEST "2 number multiplication works" = interp (Op2 (Mul, Const (Int 3), Const (Int 4))) [] = Const (Int 12)
+let%TEST "2 number division works" = interp (Op2 (Div, Const (Int 12), Const (Int 4))) [] = Const (Int 3)
+let%TEST "2 number modulo works" = interp (Op2 (Mod, Const (Int 12), Const (Int 5))) [] = Const (Int 2)
+let%TEST "2 number less than works when true" = interp (Op2 (LT, Const (Int (-10)), Const (Int 0))) [] = Const (Bool true)
+let%TEST "2 number less than works when false" = interp (Op2 (LT, Const (Int 10), Const (Int 0))) [] = Const (Bool false)
+let%TEST "2 number greater than works when true" = interp (Op2 (GT, Const (Int 5), Const (Int 3))) [] = Const (Bool true)
+let%TEST "2 number greater than works when false" = interp (Op2 (GT, Const (Int (-12)), Const (Int (-2)))) [] = Const (Bool false)
+let%TEST "2 number equal works when true" = interp (Op2 (Eq, Const (Int (-7)), Const (Int (-7)))) [] = Const (Bool true)
+let%TEST "2 number equal works when false" = interp (Op2 (Eq, Const (Int (-7)), Const (Int 7))) [] = Const (Bool false)
+let%TEST "Equality for number and bool returns false" = interp (Op2 (Eq, Const (Int 1), Const (Bool true))) [] = Const (Bool false)
+let%TEST "Equality for bools works" = interp (Op2 (Eq, Const (Bool false), Const (Bool false))) [] = Const (Bool true)
+
+(* 3 + 7 + 4 = 14 *)
+let%TEST "Adding 3 numbers works" =
+	interp (Op2 (Add, Op2 (Add, Const (Int 3), Const (Int 7)), Const (Int 4))) [] = Const (Int 14)
+(* (7 + 2) x 0 = 0 not 7 *)
+let%TEST "Order of operations by the structure of expression, not pemdas" =
+	interp (Op2 (Mul, Op2 (Add, Const (Int 7), Const (Int 2)), Const (Int 0))) [] = Const (Int 0)
+
+let%TEST "Adding bools is invalid" = test_interp_throws (Op2 (Add, Const (Bool false), Const (Bool true))) []
+let%TEST "Dividing by 0 is invalid" = test_interp_throws (Op2 (Div, Const (Int 12), Const (Int 0))) []
+let%TEST "Modding by 0 is invalid" = test_interp_throws (Op2 (Mod, Const (Int 12), Const (Int 0))) []
+
+
+(* ---------If----------- *)
+let%TEST "If invalid for non-bool conditional" =
+	test_interp_throws (If (Const (Int 0), Const (Int 1), Const (Int 2))) []
+let%TEST "If evaluates to first expression when true" =
+	interp (If (Const (Bool true), Const (Int 1), Const (Int 2))) [] = Const (Int 1)
+let%TEST "If evaluates to second expression when true" =
+	interp (If (Const (Bool false), Const (Int 1), Const (Int 2))) [] = Const (Int 2)
+let%TEST "If can have an more than a const in its conditional" =
+	interp (If (Op2 (GT, Const (Int (7)), Const (Int (4))),
+		Const (Int 1), Const (Int 2))) [] = Const (Int 2)
+
+(* -----Fun/Fix/App------- *)
+(* TODO not sure about this, returning a closure might be valid and convenient for our language *)
+(* let%TEST "Function definition alone is not a program" = test_interp_throws (Fun ("x", (Id "x"))) []
+let%TEST "Fix definition alone is not a program" = test_interp_throws (Fix ("x", (Id "x"))) []
+ *)
+
+(* --------Empty--------- *)
+(* let%TEST "Empty is an invalid return value" = test_interp_throws Empty [] idk if this is true TODO *)
 
 
 
