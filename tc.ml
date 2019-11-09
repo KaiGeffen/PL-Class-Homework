9c0aca1695aca7d01849b528dba4ac52ea40d157
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-11-09 08:40:37 -0500

    Stubbed simple types and red tested constants

--- tc.ml
+++ tc.ml
@@ -1,6 +1,18 @@
-(* The Third homework from Arjun Guha's (UMass) Fall 2018 CS631 'Programming-Languages' class *)
+(* Base on the third homework from Arjun Guha's (UMass) Fall 2018 CS631 'Programming-Languages' class *)
 (* https://people.cs.umass.edu/~arjun/courses/compsci631-fall2018/hw/tc.pdf *)
+open Tc_util
 
-let rec tc (e : exp) : bool =
+(* A type environment is a partial function from variables to types *)
+type typeEnv = (id * typ) list
+
+(* Returns the type of the given program, or fails if the given program is not type-correct *)
+(* g is the type environment in which the program is considered, stands for Gamma *)
+let rec tc (e : exp) (g : typeEnv) : typ =
   match e with 
-  | _ -> failwith "operation not defined"
+    | Const c -> failwith "not implemented"
+    | Id x -> failwith "not implemented"
+    | Op2 (op, e1, e2) -> failwith "not implemented"
+    | If (e1, e2, e3) -> failwith "not implemented"
+    | Fun (x, t, e1) -> failwith "not implemented"
+    | App (e1, e2) -> failwith "not implemented"
+    | _ -> failwith "not implemented"
--- tc_tests.ml
+++ tc_tests.ml
@@ -2,6 +2,15 @@
 module PTest = Ppx_test.Test
 
 open Tc
+open Tc_util
+
+(* Helper methods for testing *)
+let test_tc  ?(g : typeEnv = []) (prog : string) (t : typ) : bool =
+  t == tc (from_string prog) g
+
+(* -------Constants------- *)
+let%TEST "Bool has type bool" = test_tc "true" TBool && test_tc "false" TBool
+let%TEST "Integer has type integer" = test_tc "3" TInt
 
 (* Runs all tests declared with let%TEST. This must be the last line in the file. *)
 let _ = Ppx_test.Test.collect ()
