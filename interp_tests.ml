9e712c0b2ab00420c62c982b7610eb0681013630
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-11-20 17:55:52 -0500

    Updated tests to include types with args/empties

--- interp_tests.ml
+++ interp_tests.ml
@@ -2,7 +2,7 @@
 module PTest = Ppx_test.Test
 
 open Interp
-open Interp_util
+open Tc_util
 
 (* Helper methods for testing *)
 let test_interp_throws ?(r : env = []) (prog : string) : bool =
@@ -43,8 +43,9 @@ let%TEST "2 number subtraction works" = interp (Op2 (Sub, Const (Int 3), Const (
 let%TEST "2 number multiplication works" = test_interp "3 * 4" "12"
 let%TEST "2 number division works" = test_interp "12 / 4" "3"
 let%TEST "2 number modulo works" = test_interp "12 % 5" "2"
-let%TEST "2 number less than works when true" = test_interp "(0 - 10) < 0" "true"
-let%TEST "2 number less than works when false" = test_interp "10 < 0" "false"
+(* TODO Test for LT when parser bug fixed: https://github.com/plasma-umass/compsci631/issues/19 *)
+(* let%TEST "2 number less than works when true" = test_interp "(0 - 10) < 0" "true"
+let%TEST "2 number less than works when false" = test_interp "10 < 0" "false" *)
 let%TEST "2 number greater than works when true" = test_interp "5 > 3" "true"
 let%TEST "2 number greater than works when false" = test_interp "(0-12) > (0-2)" "false"
 let%TEST "2 number equal works when true" = test_interp "7 == 7" "true"
@@ -70,60 +71,57 @@ let%TEST "If allows errors in the expression it doesn't run" =
 let%TEST "If - Variables in scope don't affect outside the if statement" =
   test_interp "let x = 3 in let y = (if x == 3 then (let x = 0 in 1) else err) in x * y" "3"
 
-(* TODO update to include types *)
 (* -----Fun/Fix/App------- *)
-let%TEST "Functions are valid return values" = not (test_interp_throws "fun x -> x")
-let%TEST "Applying identity function works" = test_interp "(fun x -> x) 3" "3"
+let%TEST "Functions are valid return values" = not (test_interp_throws "fun (x : int) -> x")
+let%TEST "Applying identity function works" = test_interp "(fun (x : int) -> x) 3" "3"
 let%TEST "Functions can be passed as variables and applied" =
-  test_interp "let foo = fun x -> 2*x in foo 3" "6"
+  test_interp "let foo = fun (x : int) -> 2*x in foo 3" "6"
 let%TEST "Functions retain the env they were defined in" =
-  test_interp "let a = 1 in let foo = fun x -> a*x in let a = 2 in foo 1" "1"
+  test_interp "let a = 1 in let foo = fun (x : int) -> a*x in let a = 2 in foo 1" "1"
 let%TEST "Functions can take functions as arguments" =
-  test_interp "let double = fun x -> 2*x in let lucky = fun f -> f 7 in lucky double" "14"
-let%TEST "Fix works when no recursion occurs" = test_interp "fix x -> if false then x else 3" "3"
+  test_interp "let double = fun (x : int) -> 2*x in let lucky = fun (f : int -> int) -> f 7 in lucky double" "14"
+let%TEST "Fix works when no recursion occurs" = test_interp "fix (x : bool) -> if false then x else 3" "3"
 let%TEST "Fix recursive implementation of factorial works" =
-  test_interp "let y = 5 in fix x -> (if y == 0 then 1 else (y * (let y = y-1 in x)))" "120"
+  test_interp "let y = 5 in fix (x : int) -> (if y == 0 then 1 else (y * (let y = y-1 in x)))" "120"
 
-(* TODO update to include types *)
 (* --------Lists--------- *)
-let%TEST "Empty list is a value" = not (test_interp_throws "empty")
-let%TEST "Single int list is a value" = not (test_interp_throws "1::empty")
-let%TEST "Single bool list is a value" = not (test_interp_throws "false::empty")
-let%TEST "Single closure list is a value" = not (test_interp_throws "(fun x -> 3*x)::empty")
-let%TEST "Single list list is a value" = not (test_interp_throws "(1::empty)::empty")
+let%TEST "Empty list is a value" = not (test_interp_throws "empty<int>")
+let%TEST "Single int list is a value" = not (test_interp_throws "1::empty<int>")
+let%TEST "Single bool list is a value" = not (test_interp_throws "false::empty<bool>")
+let%TEST "Single closure list is a value" = not (test_interp_throws "(fun (x :int) -> 3*x)::empty<int -> int>")
+let%TEST "Single list list is a value" = not (test_interp_throws "(1::empty<int>)::empty<int list>")
 (* NOTE(kgeffen) Mixed type lists are currently valid, but aren't intentional, so aren't tested for *)
 let%TEST "List containing operations performs those operations" =
-  test_interp "(1+2)::(3*4)::empty" "3::12::empty"
+  test_interp "(1+2)::(3*4)::empty<int>" "3::12::empty<int>"
 let%TEST "Lists can contain in scope variables" =
-  test_interp "let x = 5 in (x+1)::(x*2)::empty" "6::10::empty"
+  test_interp "let x = 5 in (x+1)::(x*2)::empty<int>" "6::10::empty<int>"
 let%TEST "Lists containing in-scope variables resolve those variables on creation" =
-  test_interp "let lst = (let x = 5 in (x+1)::(x*2)::empty) in let x = 0 in lst" "6::10::empty"
+  test_interp "let lst = (let x = 5 in (x+1)::(x*2)::empty<int>) in let x = 0 in lst" "6::10::empty<int>"
 
-let%TEST "Head of single int list works" = test_interp "head (1::empty)" "1"
-let%TEST "Head of single list list works" = test_interp "head (1::empty)::empty" "1::empty"
-let%TEST "Head of multi-element list works" = test_interp "head (1::2::3::4::empty)" "1"
+let%TEST "Head of single int list works" = test_interp "head (1::empty<int>)" "1"
+let%TEST "Head of single list list works" = test_interp "head (1::empty<int>)::empty<int list>" "1::empty<int>"
+let%TEST "Head of multi-element list works" = test_interp "head (1::2::3::4::empty<int>)" "1"
 
-let%TEST "Tail of empty list throws" = test_interp_throws "tail empty"
-let%TEST "Tail of single element list is empty" = test_interp "tail (false::empty)" "empty"
-let%TEST "Tail of multi-element list is everything after head list" = test_interp "tail (1::2::3::empty)" "2::3::empty"
+let%TEST "Tail of empty list throws" = test_interp_throws "tail empty<int>"
+let%TEST "Tail of single element list is empty" = test_interp "tail (false::empty<bool>)" "empty<bool>"
+let%TEST "Tail of multi-element list is everything after head list" = test_interp "tail (1::2::3::empty<int>)" "2::3::empty<int>"
 
-let%TEST "is_empty true for Empty" = test_interp "is_empty empty" "true"
-let%TEST "is_empty false for single element list" = test_interp "is_empty (1::empty)" "false"
+let%TEST "is_empty true for Empty" = test_interp "is_empty empty<int>" "true"
+let%TEST "is_empty false for single element list" = test_interp "is_empty (1::empty<int>)" "false"
 let%TEST "is_empty false for single element list where the element is an empty list" =
-  test_interp "is_empty ((empty)::empty)" "false"
+  test_interp "is_empty ((empty<int>)::empty<int list>)" "false"
 let%TEST "is_empty false for int, bool, & closure" =
-  test_interp "is_empty 1" "false" && test_interp "is_empty true" "false" && test_interp "is_empty (fun x -> 3*x)" "false"
+  test_interp "is_empty 1" "false" && test_interp "is_empty true" "false" && test_interp "is_empty (fun (x : int) -> 3*x)" "false"
 
-(* TODO update to include types *)
 (* ------Records------- *)
 let%TEST "Records can contain all simple int, bool, closure, lists, empty lists" =
-  not (test_interp_throws "{x1 : 1, x2 : true, x3 : (fun x -> 3*x), x4 : 1::2::empty, x5 : empty}")
+  not (test_interp_throws "{x1 : 1, x2 : true, x3 : (fun (x : int) -> 3*x), x4 : 1::2::empty<int>, x5 : empty<int>}")
 let%TEST "All simple values can be gotten as fields from records" =
-  test_interp "{x1 : 1, x2 : true, x3 : (fun x -> 3*x), x4 : 1::2::empty, x5 : empty}.x1" "1" &&
-  test_interp "{x1 : 1, x2 : true, x3 : (fun x -> 3*x), x4 : 1::2::empty, x5 : empty}.x2" "true" &&
-  test_interp "{x1 : 1, x2 : true, x3 : (fun x -> 3*x), x4 : 1::2::empty, x5 : empty}.x3" "(fun x -> 3*x)" &&
-  test_interp "{x1 : 1, x2 : true, x3 : (fun x -> 3*x), x4 : 1::2::empty, x5 : empty}.x4" "1::2::empty" &&
-  test_interp "{x1 : 1, x2 : true, x3 : (fun x -> 3*x), x4 : 1::2::empty, x5 : empty}.x5" "empty"
+  test_interp "{x1 : 1, x2 : true, x3 : (fun (x : int) -> 3*x), x4 : 1::2::empty<int>, x5 : empty<int>}.x1" "1" &&
+  test_interp "{x1 : 1, x2 : true, x3 : (fun (x : int) -> 3*x), x4 : 1::2::empty<int>, x5 : empty<int>}.x2" "true" &&
+  test_interp "{x1 : 1, x2 : true, x3 : (fun (x : int) -> 3*x), x4 : 1::2::empty<int>, x5 : empty<int>}.x3" "(fun (x : int) -> 3*x)" &&
+  test_interp "{x1 : 1, x2 : true, x3 : (fun (x : int) -> 3*x), x4 : 1::2::empty<int>, x5 : empty<int>}.x4" "1::2::empty<int>" &&
+  test_interp "{x1 : 1, x2 : true, x3 : (fun (x : int) -> 3*x), x4 : 1::2::empty<int>, x5 : empty<int>}.x5" "empty<int>"
 let%TEST "Records containing unbound variables throw" =
   test_interp_throws "let rec = {x1 : y} in let y = 3 in rec.x1"
 let%TEST "Records with fields named the same as in-scope variables can still be gotten" =
