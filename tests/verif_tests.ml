e8dac7f0dd8e4728aede6c9cb69786d8d2b5383a
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-12-02 16:22:03 -0500

    Implemented and wrote meager temp tests for assignment for wp

--- tests/verif_tests.ml
+++ tests/verif_tests.ml
@@ -19,6 +19,11 @@ let%TEST "Wp for abort is false, no precondition makes abort valid" =
   test_wp "requires false; abort;"
 (* let%TEST "Wp for " =
   test_wp "requires true; ensures y==x0 && x==y0; x=x0; y=y0; t=x; x=y; y=t;" *)
+let%TEST "Wp for basic assignment works, TODO rework test" =
+  test_wp "requires y > 5; ensures y > x; x = 5;"
+let%TEST "Wp for basic assignment works CASE 2, TODO rework test" =
+  test_wp "requires 2 * y < 5; ensures y < 5; y = 2 * y;"
+
 
 (* Examples from slides *)
 (* let%TEST "Weakest precondition works for basic assignment program" =
--- verif.ml
+++ verif.ml
@@ -9,12 +9,36 @@ open Printf
 
 (* let solver = make_solver z3_path *)
 
+(* Replace all occurences of x throughout the given body with new_val *)
+let rec replace_in_bexp (body : bexp) (x : string) (new_val : aexp) : bexp = 
+  match body with
+    | BConst c -> BConst c
+    | BAnd (b1, b2) -> BAnd (replace_in_bexp b1 x new_val, replace_in_bexp b2 x new_val)
+    | BOr (b1, b2) -> BOr (replace_in_bexp b1 x new_val, replace_in_bexp b2 x new_val)
+    | BNot b1 -> BNot (replace_in_bexp b1 x new_val)
+    | BCmp (cmp1, a1, a2) ->
+      BCmp (
+        cmp1,
+        replace_in_aexp a1 x new_val,
+        replace_in_aexp a2 x new_val
+      )
+and replace_in_aexp (body : aexp) (x : string) (new_val : aexp) : aexp =
+  match body with
+    | AConst c -> AConst c
+    | AVar y -> if x = y then new_val else AVar y
+    | AOp (op1, a1, a2) ->
+      AOp (
+        op1,
+        replace_in_aexp a1 x new_val,
+        replace_in_aexp a2 x new_val
+      )
+
 (* Weakest preconditions *)
 let rec wp (c : cmd) (post : bexp) : bexp =
   match c with
   | CSkip -> post
   | CAbort -> BConst false
-  | CAssign (x, a) -> failwith "not implemented"
+  | CAssign (x, aexp_val) -> replace_in_bexp post x aexp_val
   | CIf (cond, c1, c2) -> failwith "not implemented"
   | CWhile (b1, b2, c1) -> failwith "not implemented"
   | CSeq (c1, c2) -> failwith "not implemented"
