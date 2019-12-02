49c68b30e08ce4cca079b0bc5460de09ee48ce12
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-12-02 11:15:34 -0500

    Commented, stubbed, opened to get wp to work in stubbed form

--- verif.ml
+++ verif.ml
@@ -1,13 +1,13 @@
 (* Base on the fourth homework from Arjun Guha's (UMass) Fall 2018 CS631 'Programming-Languages' class *)
 (* https://people.cs.umass.edu/~arjun/courses/compsci631-fall2018/hw/verif.pdf *)
+(* ocamlbuild -use-ocamlfind -pkg compsci631 -pkg ppx_test -pkg z3 -pkg unix verif.d.byte; ./verif.d.byte FILE_PATH *)
+open Smtlib
 open Imp
+open Printf
 
-let z3_path = "z3" (* Update this with the path to Z3 or to a debugging script. *)
+(* let z3_path = "/Users/kiv/.opam/4.06.1/lib/z3/" *) (* Update this with the path to Z3 or to a debugging script. *)
 
-let solver = make_solver z3_path
-
-let verify (pre : bexp) (cmd : cmd) (post : bexp) : bool =
-  failwith "not implemented"
+(* let solver = make_solver z3_path *)
 
 (* Weakest preconditions *)
 let rec wp (c : cmd) (post : bexp) : bexp =
@@ -18,6 +18,10 @@ let rec wp (c : cmd) (post : bexp) : bexp =
   | CIf (cond, c1, c2) -> failwith "not implemented"
   | CWhile (b1, b2, c1) -> failwith "not implemented"
   | CSeq (c1, c2) -> failwith "not implemented"
+
+let verify (pre : bexp) (cmd : cmd) (post : bexp) : bool =
+  let b = wp cmd post in
+  true
 
 let _ =
   let filename = Sys.argv.(1) in
