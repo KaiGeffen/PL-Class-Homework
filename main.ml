8aaa6c49c781784310db07896214dafa230060ba
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-12-16 16:47:46 -0500

    Added to tc a method from filepath to string result of type-check

--- main.ml
+++ main.ml
@@ -2,12 +2,11 @@
 open Interp
 open Tc
 (* open Verif *)
-open Tc_util
 
 let job = Sys.argv.(1);;
 let fp = Sys.argv.(2);;
 let _ = 
   if job = "interp" then print_endline (interp_file fp)
-  else if job = "tc" then print_endline (show_typ (get_type (from_file fp)))
+  else if job = "tc" then print_endline (tc_file fp)
   (* else if job = "verify" then print_endline (verify ) *)
   else failwith "Unknown job"
