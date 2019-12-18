edd8fdcb00ae36f8a1aa81e6b148bb2c102b8d37
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-12-18 00:51:05 -0500

    Updated build command for newly capitalized tests/Main.ml file

--- tests/Main.ml
+++ tests/Main.ml
@@ -1,4 +1,4 @@
-(* ocamlbuild -use-ocamlfind -pkg compsci631 -pkg ppx_test -pkg z3 -pkg unix tests/main.d.byte; ./main.d.byte *)
+(* ocamlbuild -use-ocamlfind -pkg compsci631 -pkg ppx_test -pkg z3 -pkg unix tests/Main.d.byte; ./Main.d.byte *)
 open Interp_tests
 open Tc_tests
 open Verif_tests
