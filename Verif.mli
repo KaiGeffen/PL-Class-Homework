c59ba3756fa371374024024c8faf838e2898822d
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2019-12-20 15:05:23 -0500

    Clarified comment about verification in verifier interface

--- Verif.mli
+++ Verif.mli
@@ -1,6 +1,6 @@
 (* The basic verifier for our functional language. *)
 (* Takes a program and returns if it verifies *)
-(* A program verifies if meeting its preconditions and executing it always guarantees its post-conditions *)
+(* A program verifies if, for every state which meets pre, running cmd will result in a state which meets post *)
 (* Also, all loop invariant guarantees must always be met *)
 
 (* Verify the given program, and return true if it verifies *)
