4b8174d7a54475b9b2ff8427269abdb04a445612
Kai Geffen <kiv@Kais-MacBook-Pro.local>
2020-01-01 00:08:17 -0500

    Method for exp op to assembly op

--- Compiler.ml
+++ Compiler.ml
@@ -1,11 +1,29 @@
 open Printf
 open Compiler_util2
 
+let op_exp_to_assembly (op : op2) : ILVM.op2 =
+  match op with
+    | LT -> ILVM.LT
+    | GT -> failwith "TODO assembly does not support greater than"
+    | Eq -> ILVM.Eq
+    | Add -> ILVM.Add
+    | Sub -> ILVM.Sub
+    | Mul -> ILVM.Mul
+    | Div -> ILVM.Div
+    | Mod -> ILVM.Mod
+
 let rec exp_to_assembly (e : exp) : ILVM.block list =
   match e with
   | Id x -> failwith "not implemented"
   | Const c -> failwith "not implemented"
   | Op2 (op, e1, e2) -> failwith "not implemented"
+    (* Op2 (
+      reg,
+      op_exp_to_assembly op,
+      val1,
+      val2,
+      Exit reg_value
+    ) *)
   | App (ef, e_args) -> failwith "not implemented"
   | If (e1, e2, e3) -> failwith "not implemented"
   | Let (x, e1, e2) -> failwith "not implemented"
